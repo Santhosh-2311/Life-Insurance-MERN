ae3ace27e663f0b4168cde6aaefd48f86cced1c6
Santhosh K R <santhosh2004evps@gmail.com>
2025-05-04 15:19:19 +0530

    Use REACT_APP_API_URL as base URL for all API calls in frontend

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,5 +1,5 @@
+import axios from '../axios'
 //importing the neccassary constants
-import axios from 'axios'
 import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import axios from '../axios'
 //importing the neccessary constants
 import {
   USER_DETAILS_FAIL,
